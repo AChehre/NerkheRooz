f1502041342b339370c2b78d5474ec56620d9d1f
ahmad <achehre@yahoo.com>
2026-02-18 21:48:12 +0330

    Move cron job to cron-job.org because of the cloudflare cron jobs limitations

--- apps/nerkhe-rooz-scheduler/src/index.js
+++ apps/nerkhe-rooz-scheduler/src/index.js
@@ -1,38 +1,41 @@
 import { createSupabase } from './db/supabaseClient.js';
 import { createPriceRepository } from './db/priceRepository.js';
-
-const { getPrices, getAveragePrices } = require('../../get-prices/services/getPrices');
-
-const { routes } = require('../../shared/router/routes');
-const { parseAssets } = require('../../shared/router/utils');
-
-const handlers = {
-	getPrices,
-	getAveragePrices,
-};
+import { getPrices } from '../../get-prices/services/getPrices.js';
 
 export default {
 	async fetch(request, env, ctx) {
-		// const assets1 = routes.needsAssets ? parseAssets(url.searchParams.get('assets')) : [];
+		const url = new URL(request.url);
 
-		const supabase = createSupabase(env);
-		const repo = createPriceRepository(supabase);
+		if (url.pathname === '/run') {
+			await runJob(env);
+			return new Response('Job executed');
+		}
 
-		const prices = await getPrices();
-
-		const providers = [...new Set(prices.map((p) => p[0]))];
-
-		const assets = new Set();
-		prices.forEach(([providerName, data]) => {
-			data.result.data.forEach((item) => {
-				assets.add(item.type.symbol);
-			});
-		});
-
-		await repo.upsertProviders(providers);
-		await repo.upsertAssets([...assets]);
-		await repo.insertPrices(prices);
-
-		return new Response(JSON.stringify({ ok: true }));
+		return new Response('Not found', { status: 404 });
 	},
+
+	// async scheduled(event, env, ctx) {
+	// 	ctx.waitUntil(runJob(env));
+	// },
 };
+
+async function runJob(env) {
+	console.log('Cron started at:', new Date().toISOString());
+	const supabase = createSupabase(env);
+	const repo = createPriceRepository(supabase);
+
+	const prices = await getPrices();
+
+	const providers = [...new Set(prices.map((p) => p[0]))];
+	const assets = new Set();
+
+	prices.forEach(([providerName, data]) => {
+		data.result.data.forEach((item) => {
+			assets.add(item.type.symbol);
+		});
+	});
+
+	await repo.upsertProviders(providers);
+	await repo.upsertAssets([...assets]);
+	await repo.insertPrices(prices);
+}
